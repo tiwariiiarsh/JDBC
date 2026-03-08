86f1ace41c80d849a1a6956066dcdc9cbc2b0e0a
Arsh <tiwariiiarsh1106@gmail.com>
2026-03-08 12:28:12 +0530

    insertion of data is also done

--- src/Main.java
+++ src/Main.java
@@ -19,23 +19,30 @@ public class Main {
             Connection connection = DriverManager.getConnection(url,userName,password);
 //            query will execute through statement interface
             Statement statement = connection.createStatement();
-            String query = "SELECT*FROM Students";
-//            when we retrieve data from DB through query--> use executeQuery
-//            retrieve  data stored in the form of tables  and ResultSet used to store data in tableform
-            ResultSet resultSet =    statement.executeQuery(query);
+//            String query = "SELECT*FROM Students";
+////            when we retrieve data from DB through query--> use executeQuery
+////            retrieve  data stored in the form of tables  and ResultSet used to store data in tableform
+//            ResultSet resultSet =    statement.executeQuery(query);
+//
+//            while(resultSet.next()){  //resultSet.next()--> boolean type --> return true if next row is available in row
+//                int id = resultSet.getInt("id");
+//                String name = resultSet.getString("name");
+//                int age  = resultSet.getInt("age");
+//                Double marks = resultSet.getDouble("marks");
+//                System.out.println("ID:  "+ id);
+//                System.out.println("name:  "+ name);
+//                System.out.println("Age:  "+  age);
+//                System.out.println("Marks:  "+ marks);
+//            }
 
-            while(resultSet.next()){  //resultSet.next()--> boolean type --> return true if next row is available in row
-                int id = resultSet.getInt("id");
-                String name = resultSet.getString("name");
-                int age  = resultSet.getInt("age");
-                Double marks = resultSet.getDouble("marks");
-                System.out.println("ID:  "+ id);
-                System.out.println("name:  "+ name);
-                System.out.println("Age:  "+  age);
-                System.out.println("Marks:  "+ marks);
-            }
+            String query = String.format("INSERT INTO Students(name,age,marks) VALUES('%s', %o, %f)","Alan",21,99.8);
 //            when we insert,delete,update data from DB through query--> use executeUpdate
-//            statement.executeUpdate(query);
+            int rowsAffected = statement.executeUpdate(query);
+            if(rowsAffected>0){
+                System.out.println("Data inserted successfully !!");
+            }else{
+                System.out.println("Data not inserted ");
+            }
 
 
         }catch (SQLException e){
